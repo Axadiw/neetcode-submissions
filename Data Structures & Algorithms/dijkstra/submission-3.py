192457eafd295cc89550e7335d1e38ddb8dcb335
Michał Mizera <axadiw@gmail.com>
2026-07-30 01:26:12 +0200

    Add: dijkstra - submission-3

---
+++ Data Structures & Algorithms/dijkstra/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        graph = {}
+        for i in range(n):
+            graph[i] = []
+        
+        for s,dst,weight in edges:
+            graph[s].append((weight,dst))
+        
+        ret_val = {}
+
+        heap = [(0,src)]
+
+        while heap:
+            item = heapq.heappop(heap)
+            if item[1] in ret_val:
+                continue
+
+            ret_val[item[1]] = item[0]
+            for new_item in graph[item[1]]:
+                if new_item[1] not in ret_val:
+                    heapq.heappush(heap, (new_item[0] + item[0],new_item[1]))
+
+        for i in range(n):
+            if i not in ret_val:
+                ret_val[i] = -1
+        return ret_val
+
