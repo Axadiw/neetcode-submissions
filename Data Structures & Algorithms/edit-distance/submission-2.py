6477a3cc6fcd62e03d71b4f453fcabd00bc01ad8
Michał Mizera <axadiw@gmail.com>
2026-08-09 01:07:20 +0200

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        cache = {}
+        def dfs(i1,i2):
+            if (i1,i2) in cache:
+                return cache[(i1,i2)]
+
+            if i1 >= len(word1) or i2 >= len(word2):
+                
+                cache[(i1,i2)] = abs((len(word1)-i1) - (len(word2)-i2))
+                return cache[(i1,i2)]
+
+            if word1[i1] == word2[i2]:
+                cache[(i1,i2)] = dfs(i1+1,i2+1)
+                return cache[(i1,i2)]
+
+            cache[(i1,i2)] = 1 + min(dfs(i1+1,i2+1),dfs(i1,i2+1),dfs(i1+1,i2))
+            return cache[(i1,i2)]
+        
+        return dfs(0,0)
+
+        
